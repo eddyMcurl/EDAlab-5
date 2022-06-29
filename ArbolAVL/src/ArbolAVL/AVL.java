b90cc5dd8932a5b083b6a469e42efe8a458f4f16
eddym <eddy@curl.cl>
2022-06-29 08:37:20 -0500

    agregue la clase avl

---
+++ ArbolAVL/src/ArbolAVL/AVL.java
@@ -0,0 +1,22 @@
+package ArbolAVL;
+
+public class AVL<E extends Comparable<E>>  {
+    private Node root;
+	private boolean height;
+    public AVL() {
+		this.root = null;
+	}
+	public boolean isEmpty() {
+		return this.root == null;
+	}
+    public void insert(E x) {
+	}
+    public E search(E x) {
+        return null;
+    }
+    public void remove(E x) {	
+    }
+    public String toString() {
+        return null;
+    }
+}
